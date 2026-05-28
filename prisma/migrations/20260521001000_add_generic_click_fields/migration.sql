530f9990e17acb992bc52b7a562e3232a2b636df
shreya <shree.gupta617@gmail.com>
2026-05-28 18:36:04 +0530

    Reset prisma migrations for mysql

--- prisma/migrations/20260521001000_add_generic_click_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "SubscriptionClick" ADD COLUMN "eventType" TEXT NOT NULL DEFAULT 'subscription';
-ALTER TABLE "SubscriptionClick" ADD COLUMN "itemId" TEXT;
-ALTER TABLE "SubscriptionClick" ADD COLUMN "itemTitle" TEXT;
-ALTER TABLE "SubscriptionClick" ADD COLUMN "itemUrl" TEXT;
-
--- CreateIndex
-CREATE INDEX "SubscriptionClick_shop_eventType_createdAt_idx" ON "SubscriptionClick"("shop", "eventType", "createdAt");
